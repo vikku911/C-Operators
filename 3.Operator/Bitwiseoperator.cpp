83b1ba70c7a766ceefb78292962a99c0241d9941
vikku911 <71919075+vikku911@users.noreply.github.com>
2022-11-27 15:58:32 +0530

    Add files via upload

---
+++ 3.Operator/Bitwiseoperator.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a=4;
+    int b=6;
+    
+    cout<<"a&b ="<< (a&b) <<endl;
+    cout<<"a|b ="<< (a|b) <<endl;
+    cout<<"~a  ="<< ~a <<endl;
+    cout<<"a^b ="<< (a^b) <<endl;
+}
+
+/*
+
+a&b =4
+a|b =6
+~a  =-5
+a^b =2
+
+*/
---
+++ 3.Operator/lest,rightshiftoperator.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<(5<<1)<<endl;
+    cout<<(17<<2)<<endl;
+    cout<<(15>>1)<<endl;
+    cout<<(5>>2)<<endl;
+
+}
+/*
+
+10
+68
+7
+1
+
+*/
